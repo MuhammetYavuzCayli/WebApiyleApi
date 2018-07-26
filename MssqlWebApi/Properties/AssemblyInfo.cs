3b844f4619549e23d54e0b92ce44f053f586e644
Yavuz ÇAYLI <muhammetyavuzcayli@gmail.com>
2018-07-26 11:25:16 +0300

    Code First Approach - Create Context - Fluent Api - Migrations

---
+++ MssqlWebApi/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir derlemeyle ilgili Genel Bilgiler aşağıdaki öznitelik kümesi kullanılarak 
+// denetlenir. Bir derlemeyle ilişkilendirilmiş bilgileri değiştirmek için bu
+// öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("MssqlWebApi")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MssqlWebApi")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğini false olarak ayarlarsanız COM bileşenleri bu 
+// derlemedeki türleri göremez. Bu derlemedeki bir türe COM'dan erişmeniz 
+// gerekiyorsa, o türde ComVisible özniteliğini true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'a açılmışsa aşağıdaki GUID typelib'in ID'si içindir
+[assembly: Guid("897de34e-365d-4e94-bae9-ca171637f07f")]
+
+// Bir derlemenin sürüm bilgisi aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      Alt Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilirsiniz veya Düzeltme ve Yapı Numaralarına aşağıda 
+// gösterildiği gibi '*' ile varsayılan değer atayabilirsiniz:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
